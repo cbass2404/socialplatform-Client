2c8c95c8edb434b4c4fbf670fdc715b78b23ad95
Cory Bass <cory.bass.07@gmail.com>
2020-11-30 16:27:03 -0600

    initial setup and commit

---
+++ src/components/App.js
@@ -0,0 +1,13 @@
+import Home from "./pages/home";
+import Navbar from "./nav/navBar";
+
+function App() {
+  return (
+    <div className="App">
+      <Navbar />
+      <Home />
+    </div>
+  );
+}
+
+export default App;
