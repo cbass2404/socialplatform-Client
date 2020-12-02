e150e14165566f73cb40bd691fe5466399eda7a3
Cory Bass <cory.bass.07@gmail.com>
2020-12-02 16:27:37 -0600

    added ability to delete a post

--- src/components/posts/post.js
+++ src/components/posts/post.js
@@ -4,9 +4,10 @@ import relativeTime from "dayjs/plugin/relativeTime";
 import PropTypes from "prop-types";
 
 import { connect } from "react-redux";
+import { deletePost } from "../redux/actions/dataActions";
 
+import { withStyles, Tooltip } from "@material-ui/core";
 import Grid from "@material-ui/core/Grid";
-import { withStyles } from "@material-ui/core";
 import Card from "@material-ui/core/Card";
 import CardContent from "@material-ui/core/CardContent";
 import CardMedia from "@material-ui/core/CardMedia";
@@ -14,6 +15,8 @@ import Typography from "@material-ui/core/Typography";
 import theme from "../util/theme";
 
 import ChatIcon from "@material-ui/icons/Chat";
+import EditIcon from "@material-ui/icons/Edit";
+import DeleteIcon from "@material-ui/icons/Delete";
 
 import TipButton from "../util/tipButton";
 
@@ -46,41 +49,60 @@ const Post = (props) => {
       likeCount,
       postId,
     },
+    deletePost,
   } = props;
 
   dayjs.extend(relativeTime);
 
   return (
     <Card className={classes.card}>
-      <CardMedia
-        image={imageUrl}
-        title="Profile image"
-        className={classes.image}
-      />
-      <CardContent className={classes.content}>
-        <Typography
-          variant="h5"
-          component={Link}
-          to={`/users/${handle}`}
-          color="primary"
-        >
-          {handle}
-        </Typography>
-        <Typography variant="body2" color="textSecondary">
-          {dayjs(createdAt).fromNow()}
-        </Typography>
-        <Typography variant="body1">{body}</Typography>
-        <span>{likeCount} Likes</span>
-        <TipButton tip="comments">
-          <ChatIcon color="primary" />
-        </TipButton>
-        <span>{commentCount} comments</span>
-      </CardContent>
+      <Grid container justify="space-between">
+        <CardMedia
+          image={imageUrl}
+          title="Profile image"
+          className={classes.image}
+        />
+        <Grid item>
+          <CardContent className={classes.content}>
+            <Typography
+              variant="h5"
+              component={Link}
+              to={`/users/${handle}`}
+              color="primary"
+            >
+              {handle}
+            </Typography>
+            <Typography variant="body2" color="textSecondary">
+              {dayjs(createdAt).fromNow()}
+            </Typography>
+            <Typography variant="body1">{body}</Typography>
+            <span>{likeCount} Likes</span>
+            <TipButton tip="comments">
+              <ChatIcon color="primary" />
+            </TipButton>
+            <span>{commentCount} comments</span>
+          </CardContent>
+        </Grid>
+        <Grid item>
+          {authenticated && credentials.handle === handle ? (
+            <Grid container>
+              <Grid item>
+                <Tooltip title="Delete">
+                  <Link onClick={() => deletePost(postId)}>
+                    <DeleteIcon color="secondary" />
+                  </Link>
+                </Tooltip>
+              </Grid>
+            </Grid>
+          ) : null}
+        </Grid>
+      </Grid>
     </Card>
   );
 };
 
 Post.propTypes = {
+  deletePost: PropTypes.func.isRequired,
   user: PropTypes.object.isRequired,
   post: PropTypes.object.isRequired,
   classes: PropTypes.object.isRequired,
@@ -90,4 +112,6 @@ const mapStateToProps = (state) => ({
   user: state.user,
 });
 
-export default connect(mapStateToProps)(withStyles(styles)(Post));
+export default connect(mapStateToProps, { deletePost })(
+  withStyles(styles)(Post)
+);
